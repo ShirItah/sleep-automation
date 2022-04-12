df8ae8b1f9b4be4755a3f0072bedcfe07765c39c
ShirItah <ishir@itamar-medical.com>
2022-04-12 15:04:23 +0300

    adding thresholds function

--- xml_stages.py
+++ xml_stages.py
@@ -3,8 +3,9 @@ import xml.etree.ElementTree as ET
 
 
 class XML_STAGES(XML_READ):
-    def __init__(self, zip_folder, xml_tags):
+    def __init__(self, zip_folder, xml_tags, thresholds_dct):
         super().__init__(zip_folder, xml_tags)
+        self.thresholds_dct = thresholds_dct
 
     def parse_xml(self):
         mytree = ET.parse(self.zip_folder)
@@ -13,4 +14,26 @@ class XML_STAGES(XML_READ):
             x = myroot.findall(tag)
             self.results_dict[tag[6:]] = x[0].text      # tag[6:] is for cutting the './/{*}' in the start of tag
         self.results_dict = {f'{k}[%]': v for k, v in self.results_dict.items()}            # adding units (percentage)
+        for k, v in self.results_dict.items():
+            res_dict = self.check_threshold_main_report(k, v, self.thresholds_dct)
+        return res_dict
+
+
+    def check_threshold_main_report(self, k, v, th_dct):
+        """
+        this function checks for each key in the following cases if they are above/below a threshold
+        :param k: key (field) in statistics.xml file
+        :param v: the appropriate value in statistics.xml file
+        :param th_dct: dict of thresholds to check on the specific fields
+        :return: dict of the parameters in statistics.xml with notes on the thresholds
+        """
+        match k:
+            case 'Wake[%]':
+                # more than 30%
+                if float(v) > th_dct['Wake[%]']:
+                    self.results_dict['Wake[%]'] = [v, 'Wake[%] Problem']
+            case 'REM[%]':
+                # less than 5%
+                if float(v) < th_dct['REM[%]']:
+                    self.results_dict['REM[%]'] = [v, 'REM[%] Problem']
         return self.results_dict
