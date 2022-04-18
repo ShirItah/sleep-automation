33f39f1dacd69ffc7df3e5c46a3235422d1c233b
ShirItah <ishir@itamar-medical.com>
2022-04-18 11:23:20 +0300

    read zpt files

--- ReadZPT.py
+++ ReadZPT.py
@@ -1,27 +1,27 @@
+# Read a binary file into a byte array
+# The format is little-endian
 import numpy as np
-import pandas as pd
 
-# convert hexadecimal to decimal
-# Create a dtype with the binary data format and the desired column names
-try:
-    dt = np.dtype('B')
-    data = np.fromfile("C:\\Users\\ishir\\PycharmProjects\\sleep-automation\\PatAmplitude.zpt", dtype=dt)
-    df = pd.DataFrame(data)
-    print(df)
-except IOError:
-    print("Error while opening the file!")
+FileName = "PatAmplitude.zpt"
 
-import numpy as np
-dtype = np.dtype('B')
-try:
-    with open("C:\\Users\\ishir\\PycharmProjects\\sleep-automation\\PatAmplitude.zpt", "rb") as f:
-        numpy_data = np.fromfile(f, dtype)
-    print(numpy_data)
-except IOError:
-    print('Error While Opening the file!')
 
-# f = open("C:\\Users\\ishir\\PycharmProjects\\sleep-automation\\PatAmplitude.zpt", "rb")
-# import array
-# a = array.array("L")  # L is the typecode for uint32
-# a.fromfile(f, 3)
-# print(a)
+def main():
+    """
+    Main function
+    """
+    get_bin_data(FileName)
+
+
+def get_bin_data(filename):
+    with open(".\\" + filename, "rb") as f:
+        data_array = np.fromfile(f, dtype=np.int16)
+        # The data is presented from byte 128 till the end.
+        # Every two bytes represent a value, so we need to start from byte 128/2
+        data_array = data_array[64:]
+        print(data_array)
+    # data_array = np.fromfile(".\\" + filename, dtype=np.int16)
+    # data_array = data_array[64:]
+    # print(data_array)
+
+if __name__ == '__main__':
+    main()
