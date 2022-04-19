fed86a8aace472c1188331b962fcd68c2478a3b6
ShirItah <ishir@itamar-medical.com>
2022-04-19 12:05:03 +0300

    updating ZPT class

--- xml_parser_main.py
+++ xml_parser_main.py
@@ -9,13 +9,12 @@ import json
 from xml_stat import XML_STAT
 from xml_report import XML_REPORT
 from xml_stages import XML_STAGES
-from ZPTclass import READ_ZPT
+from ZPTclass import ZPTclass
 from xml_reg import XML_REG
 import os
 import zipfile
 from pprint import pprint
 import csv
-import ZPTclass
 ####### Global variables #########
 
 ## Parameters lists in statistics.xml - "analysis" tag
@@ -69,24 +68,25 @@ MAINREPORT_NAME_TAGS = ("MainReport.xml", [".//{*}StudyDate", ".//{*}StartStudy"
 SLEEP_STAGES_NAME_TAGS = ('SleepStagesChart.xml', [".//{*}Wake", ".//{*}Sleep", ".//{*}REM", ".//{*}NREM",
                                                    ".//{*}Light", ".//{*}Deep"])
 
-# Regular files: contain vector of the values Vs time
-# file1 - Heart Rate Vs Time
-HR_NAME_TAGS = ("HeartRateTimeline.xml", [".//{*}SiTime", ".//{*}SiValue1"])
-
-# file2 - PAT amplitude Vs Time
-PATAMP_NAME_TAGS = ("PatAmplitudeTimeline.xml", [".//{*}SiTime", ".//{*}SiValuePatAmpl"])
-
-# file3 - Saturation Vs Time
-SAT_NAME_TAGS = ("SatPulseRate.xml", [".//{*}SiTime", ".//{*}SiValueSaO2"])
-
-# file 4- Snoring Vs Time - for studies with SBP
-SNORE_NAME_TAGS = ("SnoreBodyPositionTimeline.xml", [".//{*}SiTime", "//{*}SiValueSnore"])
-
-# file5- Body position Vs Time - TODO: understand how to add this only to the studies containing SBP
-
-# list of pair, XML File Names & Tags
-# this list is only for the regular files
-XML_REG_FILE_NAMES_AND_TAGS = [HR_NAME_TAGS, PATAMP_NAME_TAGS, SAT_NAME_TAGS, SNORE_NAME_TAGS]
+# # Regular files: contain vector of the values Vs time
+# parsing zpt files so maybe wont need these files
+# # file1 - Heart Rate Vs Time
+# HR_NAME_TAGS = ("HeartRateTimeline.xml", [".//{*}SiTime", ".//{*}SiValue1"])
+#
+# # file2 - PAT amplitude Vs Time
+# PATAMP_NAME_TAGS = ("PatAmplitudeTimeline.xml", [".//{*}SiTime", ".//{*}SiValuePatAmpl"])
+#
+# # file3 - Saturation Vs Time
+# SAT_NAME_TAGS = ("SatPulseRate.xml", [".//{*}SiTime", ".//{*}SiValueSaO2"])
+#
+# # file 4- Snoring Vs Time - for studies with SBP
+# SNORE_NAME_TAGS = ("SnoreBodyPositionTimeline.xml", [".//{*}SiTime", "//{*}SiValueSnore"])
+#
+# # file5- Body position Vs Time
+#
+# # list of pair, XML File Names & Tags
+# # this list is only for the regular files
+# XML_REG_FILE_NAMES_AND_TAGS = [HR_NAME_TAGS, PATAMP_NAME_TAGS, SAT_NAME_TAGS, SNORE_NAME_TAGS]
 
 ## Thresholds
 # Statistics Thresholds
@@ -123,6 +123,9 @@ STUDY_ZIP_ENDING = '\\study.zip'
 ## CSV file name
 CSVFILE = 'studies_params.csv'
 
+## ZPT data file name
+ZPT_TXT = 'zpt_data.txt'
+
 
 def main():
     """
@@ -133,18 +136,18 @@ def main():
     studies_dicts_list = parse_xmls(list_of_studies_xml_obj)
     # pprint(studies_dicts_list)
     read_zpt(ROOTDIR, ZPT_FILES)
-    x = ZPTclass()
+    # x = ZPTclass()
     save_csv(studies_dicts_list, CSVFILE)
 
 
-def unzip_xmls(ROOTDIR):
+def unzip_xmls(rootdir):
     """
     This function unzip xmls
-    WPI extract to ROOTDIR files for each study, which contain reprotfiles.zip file
+    WPI extract to rootdir files for each study, which contain reprotfiles.zip file
     This function first unzip reporotfiles.zip
     then it unzip study.zip file that was extracted from reportfiles.zip
     """
-    for subdir, dirs, files in os.walk(ROOTDIR):
+    for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             zip_folder = os.path.join(subdir, file)  # path of the files extracted by WPI + FileName
             zip_path = os.path.dirname(zip_folder)  # target dir for xml files
@@ -158,11 +161,11 @@ def unzip_xmls(ROOTDIR):
                 zip_ref.extractall(zip_path)
 
 
-def get_object_list(ROOTDIR):
+def get_object_list(rootdir):
     """
     this function creates list of xml objects
     according to their type it calls the specific class and creates the object
-    @param ROOTDIR: the path of all the xml files
+    @param rootdir: the path of all the xml files
     @returns list of lists xml objects (for all the studies, each list is for different study)
     """
     list_of_xml_objs = list()  # list of xml objects per study
@@ -170,7 +173,7 @@ def get_object_list(ROOTDIR):
     list_of_studies_xml_obj = list()  # list of lists - each list is for one study
 
     # for loop to read file by file
-    for subdir, dirs, files in os.walk(ROOTDIR):
+    for subdir, dirs, files in os.walk(rootdir):
         list_of_xml_objs.clear()
         if not files == []:
             for file in files:
@@ -196,18 +199,18 @@ def get_object_list(ROOTDIR):
     return list_of_studies_xml_obj
 
 
-def parse_xmls(list_of_studies_xml_obj):
+def parse_xmls(studies_xml_obj):
     """
     this function parses the necessary data from each xml according to def parse_xml in each class
     all the data is kept in dictionary; different dict for each class
     it combines all the dictionaries for each study and creates list of dicts
-    @param list_of_studies_xml_obj: the list of all the xml objects
+    @param studies_xml_obj: the list of all the xml objects
     @return: list of dicts contains the extracted data
     """
     studies_dicts_list = list()
     dest_dict = {}
     copy_dest_dict = {}
-    for list_of_xml_objs in list_of_studies_xml_obj:
+    for list_of_xml_objs in studies_xml_obj:
         dest_dict.clear()
         for xml_obj in list_of_xml_objs:
             result_dict = xml_obj.parse_xml()
@@ -218,29 +221,32 @@ def parse_xmls(list_of_studies_xml_obj):
     return studies_dicts_list
 
 
-def read_zpt(ROOTDIR, zpt_files):
-    DataMat = []
-    study_zpt_mat = []
-    for subdir, dirs, files in os.walk(ROOTDIR):
-        DataMat.clear()
+def read_zpt(rootdir, zpt_files):
+    data_mat = []
+    ZPT_FILES = ["Actigraph.zpt", "PAT_Infra.zpt", "PeripheralBP.zpt", "SaO2.zpt", "PatAmplitude.zpt"]
+
+    for subdir, dirs, files in os.walk(rootdir):
+        data_mat.clear()
         if not files == []:
+            for file in files:
+                if file in ZPT_FILES:
+                    print(file)
+
+
             for zpt_file in zpt_files:
                 for file in files:
+                    if file != zpt_file:
+                        continue
+
                     zip_folder = os.path.join(subdir, file)  # path of the files extracted by WPI+FileName
-                    if not file.endswith('.zpt'):
-                        continue
-                    if file == zpt_file:
-                        zpt_data = READ_ZPT(zip_folder, subdir, DataMat)
-                        zpt_mat = zpt_data.get_bin_data()
-                        # print(zpt_mat)
-                        # study_zpt_mat.append(zpt_mat)
-                        # zpt_data.write_zpt_to_txt()
-                    else:
-                        continue
+                    zpt_data = ZPTclass(zip_folder, subdir, data_mat, ZPT_TXT)
+                    # if file == zpt_file:
+                    #     zpt_data = ZPTclass(zip_folder, subdir, data_mat, ZPT_TXT)
+                    # else:
+                    #     continue
 
 
-
-def save_csv(studies_dicts_list, csv_file_name):
+def save_csv(studies_list, csv_file_name):
     """
     this function saves the results_dict in csv file
     in ths csv file: each column is a key; each row is a study; each cell contains the val of the key
@@ -248,12 +254,12 @@ def save_csv(studies_dicts_list, csv_file_name):
     :param studies_dicts_list: list of dictionaries for all the studies
     """
     with open('dict.json', 'w') as d_j:
-        json.dump(studies_dicts_list, d_j, indent=6)
-    fieldnames = list(studies_dicts_list[1].keys())
+        json.dump(studies_list, d_j, indent=6)
+    fieldnames = list(studies_list[1].keys())
     with open(csv_file_name, 'w', encoding='UTF8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
-        for study_param in studies_dicts_list:
+        for study_param in studies_list:
             writer.writerow(study_param)
 
 
